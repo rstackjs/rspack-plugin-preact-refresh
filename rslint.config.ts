f67ad8d17e7847c203960d81a2d4708112766496
neverland <chenjiahan.jait@bytedance.com>
2026-08-20 14:47:18 +0800

    chore(deps): upgrade Rslint to v0.8.1

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,14 +1,34 @@
-import { defineConfig, globalIgnores, js, ts } from '@rslint/core';
+import { defineConfig, globalIgnores, globals, js, ts } from '@rslint/core';
 
 export default defineConfig([
   globalIgnores(['test/hotCases/**']),
   js.configs.recommended,
   ts.configs.recommended,
   {
-    files: ['client/**/*', 'test/**/*'],
+    files: ['client/**/*'],
     rules: {
       '@typescript-eslint/no-require-imports': 'off',
       'no-undef': 'off',
+    },
+  },
+  {
+    files: ['test/**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.rstest,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+  {
+    files: ['test/configCases/**/*.js'],
+    languageOptions: {
+      globals: {
+        __webpack_modules__: 'readonly',
+      },
     },
   },
 ]);
