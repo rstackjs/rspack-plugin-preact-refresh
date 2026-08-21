6878048975f01c93ca72beeec3a809d98d8a7c16
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-08-21 16:42:37 +0800

    chore(deps): upgrade Rslint to v0.8.1 (#85)

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,3 +1,18 @@
-import { defineConfig, ts } from '@rslint/core';
+import { defineConfig, globalIgnores, js, ts } from '@rslint/core';
 
-export default defineConfig([ts.configs.recommended]);
+export default defineConfig([
+  globalIgnores(['test/hotCases/**']),
+  js.configs.recommended,
+  ts.configs.recommended,
+  {
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: ['client/**/*', 'test/**/*'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+]);
